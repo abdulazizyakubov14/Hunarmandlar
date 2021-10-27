60a1f5def883acba532caac5b9996bdf7a815d47
Mr-Muhammadjon <76444665+Mr-Muhammadjon@users.noreply.github.com>
2021-10-27 10:32:43 +0500

    commit

--- hunarmandlar/main/urls.py
+++ hunarmandlar/main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .import views
+from . import views
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name=''),
+    path('', views.HomeView.as_view(), name='home'),
 ]
