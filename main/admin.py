de47d9fea539d50f6ae2a256fe89ac8a81da75a2
Abdulhamid51 <73017095+Abdulhamid51@users.noreply.github.com>
2021-10-27 22:29:54 +0500

    shu endi

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-@admin.register(Category)
-class CatAdmin(admin.ModelAdmin):
-    list_display = ['title','slug']
-    prepopulated_fields = {'slug':('title',)}
--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path("category/<slug>", views.cat_detail, name="cat_detail"),
-]
