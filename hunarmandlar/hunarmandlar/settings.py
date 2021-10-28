e655dbd33ef6bd31aaba43a5b9cb1d7d7b39854d
Mr-Muhammadjon <76444665+Mr-Muhammadjon@users.noreply.github.com>
2021-10-28 16:55:54 +0500

    shu

--- hunarmandlar/hunarmandlar/settings.py
+++ hunarmandlar/hunarmandlar/settings.py
@@ -167,6 +167,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR,'loacle')
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
